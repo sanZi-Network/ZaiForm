964fc8b56c3a30309f6669763646d8470c63d538
000hen <hen20090325@gmail.com>
2022-06-18 11:11:54 +0800

    First commit with script initialization

---
+++ main.js
@@ -0,0 +1,51 @@
+/*
+ * ZaiForm Server Main Script
+ * Created by: Muisnow <muisnowbusiness@gmail.com>
+ *
+ * Copyright 2022 The ZaiForm Authors.
+ * Copyright 2022 Muisnow Developer.
+ * 
+ * Repository: https://github.com/sanZi-Network/ZaiForm
+ */
+
+const app = require('express')();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const { generateKeyPairSync } = require('crypto');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 1287;
+
+if (!fs.existsSync("storaged")) fs.mkdirSync("storaged");
+if (!fs.existsSync("storaged/authPrivate.key") || !fs.existsSync("storaged/authPublic.key")) {
+    const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+        modulusLength: 4096,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem'
+        }
+    });
+    fs.writeFileSync("storaged/authPrivate.key", privateKey);
+    fs.writeFileSync("storaged/authPublic.pem", publicKey);
+}
+
+app.listen(PORT, () => {
+    console.log("".padStart(60, '='));
+    console.log("\x1b[32m\x1b[5m" + "[Server Started".padStart(37, " ") + "]\x1b[0m");
+    console.log();
+    console.log(`      * Listening on port:`.padEnd(30, " ") + `${PORT}`);
+    console.log("      * Server is running on:".padEnd(30, " ") + `http://localhost:${PORT}`);
+    console.log();
+    console.log("".padStart(60, '='));
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
